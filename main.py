a6712b011ac3eb10ebf2a3f3526ea79df3da169d
Cameron Merrill <cmm2@illinois.edu>
2017-09-15 17:08:11 -0500

    Top 100 finished

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import auth
 import tweepy
+import top100
 
 consumer_key = auth.key
 consumer_secret = auth.secret
 access_token = auth.token
 access_token_secret = auth.tokenSecret
+userlist = top100.userlist
+
 
 
 
@@ -14,6 +17,9 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 # Creating the API object while passing in auth information
 api = tweepy.API(auth) 
+for user in userlist:
+	api.user_timeline(user)
+	
 
 
 
