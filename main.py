678c7679f46704d84149539b541031601aa6a945
Cameron Merrill <cmm2@illinois.edu>
2017-09-18 11:33:17 -0500

    Scraping is done

--- main.py
+++ main.py
@@ -22,9 +22,34 @@ for user in userlist:
 		timeline = api.user_timeline(user,count=200)
 		print user
 		userTweetCount = 0
+		userRetweetTotal = 0
+		userFavoriteTotal = 0
+		userRepliesTotal = 0
+
+		firstTweet =  timeline[0]
+		firstTweetDate = firstTweet.created_at
+	
 		for tweet in timeline:
-			print tweet.retweet_count 
-		print ( "---" )
+			userRetweetTotal += tweet.retweet_count
+			userFavoriteTotal += tweet.favorite_count
+			#replies is tricky, do later
+			userTweetCount += 1
+		lastTweet = timeline[userTweetCount-1]
+		lastTweetDate = lastTweet.created_at
+		delta = firstTweetDate - lastTweetDate
+		days = delta.days
+
+		print "Most Recent Tweet: " + str(firstTweetDate)
+		print "Last Crawled Tweet Date: " + str(lastTweetDate)
+		print "Days between: " + str(delta.days) 
+		print "Avg Tweets per day: " + str(float(userTweetCount)/delta.days)
+		print "Tweet Total: " + str(userTweetCount)
+		print "Retweet Total: " + str(userRetweetTotal)
+		print "Favorite Total: " + str(userFavoriteTotal)
+		print "Average Favorites per Tweet: " + str(float(userFavoriteTotal/userTweetCount)
+		print "Average Retweets per Tweet: " + str(float(userRetweetTotal/userTweetCount)
+		print ("------")
+		
 	except tweepy.error.TweepError:
 		print "Error for" + user
 	
