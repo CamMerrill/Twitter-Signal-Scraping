0d20a04a033efe7c60869852ae5f6a0cfe51e520
Cameron Merrill <cmm2@illinois.edu>
2017-09-21 10:26:31 -0500

    List cleaning

--- Lists/listCleaner.py
+++ Lists/listCleaner.py
@@ -1,13 +1,16 @@
-fileName = raw_input("Enter file name to clean: ")
-inputFile = open(fileName, 'r')
-outputFile = open(fileName + "-clean", 'w')
+#fileName = raw_input("Enter file name to clean: ")
+fileName = "US100BrandsRaw"
+inputFile = open(fileName+".txt", 'r')
+outputFile = open(fileName + "-clean.txt", 'w')
+
 
 
 for line in inputFile:
 	atIndex = line.find("@")
 	if atIndex != -1:
 		#We have a handle
-		subLine = line[:atIndex]
-		print subLine
-
+		subLine = line[atIndex:]
+		endIndex = subLine.find(")")
+		subLine = line[atIndex+1:endIndex+atIndex]
+		outputFile.write(subLine + "\n")
 
